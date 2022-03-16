2c60cede705aca451d8b31f52bfb6526acda5537
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2022-03-16 20:18:46 +0000

    add asking questions functionality and also add a function that helps to check the end of questions or not

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 question_bank=[]
 for question in question_data:
   text=question['text']
   answer=question['answer']
   questionob=Question(text,answer)
   question_bank.append(questionob)
-print(question_bank)
+quiz=QuizBrain(question_bank)
+while(quiz.still_has_questions()):
+  quiz.next_question()
