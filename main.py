b0ebd67e14a0ad55da4783a3e9b80f98f34af3b6
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2022-03-16 20:59:54 +0000

    this is to finalize the project

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ for question in question_data:
 quiz=QuizBrain(question_bank)
 while(quiz.still_has_questions()):
   quiz.next_question()
+
+print("You have completed the quiz")
+print(f"your final score was: {quiz.score}/{quiz.question_number}")
